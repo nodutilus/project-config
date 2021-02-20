09c398face2bb677dd0997ade1d55a0ce0c096b7
IgorNovozhilov <maildrakon@gmail.com>
2021-02-20 22:34:46 +0300

    test

--- setup-node/setup-node.cjs
+++ setup-node/setup-node.cjs
@@ -1,13 +1,37 @@
 const { execSync } = require('child_process')
 
-execSync('npm install @actions/core@latest github:actions/setup-node#main', {
+execSync('npm install ' +
+  '@actions/core@latest ' +
+  '@actions/github@latest ' +
+  'github:actions/setup-node#main', {
   cwd: __dirname,
   encoding: 'utf-8',
   stdio: ['inherit', 'inherit', 'inherit']
 })
 
-const core = require('@actions/core');
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-core.info(`'node-version: ${core.getInput('node-version')}`)
-core.info('run setup-node/dist/index.js:')
-require('setup-node/dist/index.js')
+const octokit = github.getOctokit()
+
+async function run() {
+  core.info(`'node-version: ${core.getInput('node-version')}`)
+
+  const latestRelease = await octokit.repos.getLatestRelease({
+    owner: 'nodejs',
+    repo: 'node'
+  })
+
+  core.info(JSON.stringify(latestRelease))
+
+  core.info('run setup-node/dist/index.js:')
+  require('setup-node/dist/index.js')
+
+  execSync('node --version', {
+    cwd: __dirname,
+    encoding: 'utf-8',
+    stdio: ['inherit', 'inherit', 'inherit']
+  })
+}
+
+run()
