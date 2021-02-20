45ece7ee70eb14f2284481bd3c9b814f5baf3054
IgorNovozhilov <maildrakon@gmail.com>
2021-02-20 22:05:00 +0300

    test

--- setup-node/setup-node.cjs
+++ setup-node/setup-node.cjs
@@ -1,6 +1,7 @@
 const { execSync } = require('child_process')
 
 execSync('npm install @actions/core@latest github:actions/setup-node#main', {
+  cwd: __dirname,
   encoding: 'utf-8',
   stdio: ['inherit', 'inherit', 'inherit']
 })
