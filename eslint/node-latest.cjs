a08dc17d64aa6f2c08b7aabdaba6b36fd3d339f4
IgorNovozhilov <maildrakon@gmail.com>
2021-02-20 19:50:17 +0300

    test

--- eslint/node-latest.cjs
+++ eslint/node-latest.cjs
@@ -1,4 +1,13 @@
 module.exports = {
   extends: ['./node-base.cjs'],
-  parser: '@babel/eslint-parser'
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        '@babel/plugin-proposal-class-properties'
+      ]
+    },
+  }
 }
