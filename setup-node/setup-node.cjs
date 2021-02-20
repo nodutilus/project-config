d98b7e6ea8fa9d29692b48e61440a7bd4df41e48
IgorNovozhilov <maildrakon@gmail.com>
2021-02-20 22:50:52 +0300

    tests

--- setup-node/setup-node.cjs
+++ setup-node/setup-node.cjs
@@ -1,8 +1,12 @@
 const { execSync } = require('child_process')
+const { get } = require('https')
+const { promisify } = require('util')
+
+const pGET = promisify(get)
 
 execSync('npm install ' +
   '@actions/core@latest ' +
-  '@actions/github@latest ' +
+  // '@actions/github@latest ' +
   'github:actions/setup-node#main', {
   cwd: __dirname,
   encoding: 'utf-8',
@@ -10,19 +14,23 @@ execSync('npm install ' +
 })
 
 const core = require('@actions/core')
-const github = require('@actions/github')
+// const github = require('@actions/github')
 
-const octokit = github.getOctokit(github.context.token)
+// const octokit = github.getOctokit(github.context.token)
 
 async function run() {
   core.info(`'node-version: ${core.getInput('node-version')}`)
 
-  const latestRelease = await octokit.repos.getLatestRelease({
-    owner: 'nodejs',
-    repo: 'node'
-  })
+  // const latestRelease = await octokit.repos.getLatestRelease({
+  //   owner: 'nodejs',
+  //   repo: 'node'
+  // })
+
+  const latestRelease = await pGET('https://api.github.com/repos/nodejs/node/releases/latest')
 
   core.info(JSON.stringify(latestRelease))
+
+  process.env[`INPUT_${'node-version'.replace(/ /g, '_').toUpperCase()}`] = latestRelease
 
   core.info('run setup-node/dist/index.js:')
   require('setup-node/dist/index.js')
