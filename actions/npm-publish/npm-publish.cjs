fb677835a019d74abbed2481083890708b5cedc2
Igor Novozhilov <maildrakon@gmail.com>
2021-04-15 13:21:41 +0300

    test

--- actions/npm-publish/npm-publish.cjs
+++ actions/npm-publish/npm-publish.cjs
@@ -2,16 +2,10 @@ const { execSync } = require('child_process')
 const { join } = require('path')
 const core = require('@actions/core')
 
-const token = core.getInput('token', { required: true })
-
-if (!token) {
-  throw new Error('NPM access token is not configured in secret variable `NPM_TOKEN`')
-}
-
 execSync(`node ${join(__dirname, 'npm-publish')}`, {
   encoding: 'utf-8',
   stdio: ['inherit', 'inherit', 'inherit'],
   env: Object.assign({}, process.env, {
-    NPM_TOKEN: token
+    NPM_TOKEN: core.getInput('token', { required: true })
   })
 })
