2be8a0010d1177e87b36b37fe9986c16448cb930
IgorNovozhilov <maildrakon@gmail.com>
2021-06-11 00:36:26 +0300

    test

---
+++ bin/test.js
@@ -0,0 +1,30 @@
+import { execSync } from 'child_process'
+import { readFileSync } from 'fs'
+
+const scripts = JSON.parse(readFileSync('./package.json', 'utf-8')).scripts || {}
+let command
+
+if ('pre-test' in scripts) {
+  command = 'npm run pre-test'
+  console.log(command)
+  execSync(command, {
+    encoding: 'utf-8',
+    stdio: ['inherit', 'inherit', 'inherit']
+  })
+}
+
+command = 'test' in scripts ? 'npm run test' : 'node test'
+console.log(command)
+execSync(command, {
+  encoding: 'utf-8',
+  stdio: ['inherit', 'inherit', 'inherit']
+})
+
+if ('post-test' in scripts) {
+  command = 'npm run post-test'
+  console.log(command)
+  execSync(command, {
+    encoding: 'utf-8',
+    stdio: ['inherit', 'inherit', 'inherit']
+  })
+}
