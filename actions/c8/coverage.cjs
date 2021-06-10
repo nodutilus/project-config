d881f18b7d0eb0d0c539532c1cc41a3b041629fe
IgorNovozhilov <maildrakon@gmail.com>
2021-06-11 00:17:22 +0300

    test

--- actions/c8/coverage.cjs
+++ actions/c8/coverage.cjs
@@ -1,4 +1,11 @@
-import '../../bin/c8.js'
+const { execSync } = require('child_process')
+const { join } = require('path')
+
+execSync(`node ${join(__dirname, '../../bin/c8')}`, {
+  encoding: 'utf-8',
+  stdio: ['inherit', 'inherit', 'inherit']
+})
+
 
 // const { execSync } = require('child_process')
 // const { readFileSync } = require('fs')
--- actions/setup-node-latest/setup-node-latest.cjs
+++ actions/setup-node-latest/setup-node-latest.cjs
@@ -30,6 +30,7 @@ async function run() {
   }
 
   core.info('run github:actions/setup-node#main')
+  // @ts-ignore
   require('@actions/setup-node/dist/index.js')
 }
 
