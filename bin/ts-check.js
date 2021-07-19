b85a39b19fd38e79a26c1e00a08e3cae4d83bdee
IgorNovozhilov <maildrakon@gmail.com>
2021-07-20 01:08:24 +0300

    Fix ts-check action

--- bin/ts-check.js
+++ bin/ts-check.js
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process'
 
-console.log('npx tsc --allowJs --checkJs --noEmit --lib ESNext --project .')
-execSync('npx tsc --allowJs --checkJs --noEmit --lib ESNext --project .', {
+console.log('npx tsc --allowJs --checkJs --noEmit --project .')
+execSync('npx tsc --allowJs --checkJs --noEmit --project .', {
   encoding: 'utf-8',
   stdio: ['inherit', 'inherit', 'inherit']
 })
