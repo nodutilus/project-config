d2d3d9e931ebb955f0ae9b7d8d60f74abffc9e7f
IgorNovozhilov <maildrakon@gmail.com>
2022-02-04 00:55:06 +0300

    support 'build' command in scripts for setup-npm

--- actions/setup-npm/setup-npm.cjs
+++ actions/setup-npm/setup-npm.cjs
@@ -1,7 +1,20 @@
 const { execSync } = require('child_process')
+const { readFileSync } = require('fs')
+
+const scripts = JSON.parse(readFileSync('./package.json', 'utf-8')).scripts || {}
+let command
 
 console.log('npm install')
 execSync('npm install', {
   encoding: 'utf-8',
   stdio: ['inherit', 'inherit', 'inherit']
 })
+
+if ('build' in scripts) {
+  command = 'npm run build'
+  console.log(command)
+  execSync(command, {
+    encoding: 'utf-8',
+    stdio: ['inherit', 'inherit', 'inherit']
+  })
+}
