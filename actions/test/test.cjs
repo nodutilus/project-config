2e1577aca369ca16f1a4cba402482644f7435914
IgorNovozhilov <maildrakon@gmail.com>
2023-03-25 14:11:44 +0300

    +

--- actions/test/test.cjs
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process')
-const { join } = require('path')
-
-execSync(`node ${join(__dirname, '../../bin/test')}`, {
-  encoding: 'utf-8',
-  stdio: ['inherit', 'inherit', 'inherit']
-})
